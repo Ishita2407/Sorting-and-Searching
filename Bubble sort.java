907809c68a1cc6cc48e220a43d54b0d79e494d7e
ISHITA <91796142+Ishita2407@users.noreply.github.com>
2024-09-21 23:02:14 +0530

    Update Bubble sort.java

--- Bubble sort.java
+++ Bubble sort.java
@@ -1,1 +1,17 @@
-
+class Solution{
+    //Function to sort the array using bubble sort algorithm.
+	public static void bubbleSort(int arr[], int n){
+        for (int i = n - 1; i >= 0; i--) {
+            int didSwap = 0;  // Properly declare the variable didSwap
+            for (int j = 0; j <= i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                    didSwap = 1;  
+                }
+            }
+            if (didSwap == 0) break;  
+        }
+    }
+}
